e66965540fa8c8d176901ee4823b3ad32d883e15
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-11 16:16:59 +0700

    Initial commit (unfinished)

---
+++ lib/App/pause.pm
@@ -0,0 +1,20 @@
+package App::pause;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A CLI for PAUSE
+
+=head1 DESCRIPTION
+
+This module is just an empty one to ease installation of this distribution (e.g.
+C<< cpanm -n App::pause >>). The real implementation is in
+L<WWW::PAUSE::Simple>. The CLI is L<pause>.
+
+
+=head1 SEE ALSO
+
+L<WWW::PAUSE::Simple>
+
+L<pause>
