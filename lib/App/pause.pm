5520be703663c2bc8e67fc88c31f6bd19c2724b6
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-16 13:35:25 +0700

    Update decsription for App::pause

--- lib/App/pause.pm
+++ lib/App/pause.pm
@@ -8,10 +8,6 @@ package App::pause;
 
 =head1 DESCRIPTION
 
-This module is just an empty one to ease installation of this distribution (e.g.
-C<< cpanm -n App::pause >>). The real implementation is in
-L<WWW::PAUSE::Simple>. The CLI is L<pause>.
-
 
 =head1 SEE ALSO
 
