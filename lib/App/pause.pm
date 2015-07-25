984aa7d21dbf523ef2efc52690b12f6dc2ca4e43
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-07-25 13:58:55 +0700

    [ux] Synchronize version of 'pause --version' with WWW::PAUSE::Simple, to avoid confusing users

--- lib/App/pause.pm
+++ lib/App/pause.pm
@@ -4,6 +4,16 @@ package App::pause;
 # DIST
 # VERSION
 
+require WWW::PAUSE::Simple;
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    # to avoid confusing users when it does 'pause --version'
+    entity_v => $WWW::PAUSE::Simple::VERSION,
+};
+
 1;
 # ABSTRACT: A CLI for PAUSE
 
