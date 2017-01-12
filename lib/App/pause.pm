58d0668d84a6e02cf67030d6dc29b1bcf17514e3
perlancar (@pc-office) <perlancar@gmail.com>
2017-01-12 21:17:46 +0700

    Move CLI attrs to lib/App/pause.pm

--- lib/App/pause.pm
+++ lib/App/pause.pm
@@ -4,10 +4,39 @@ package App::pause;
 # DIST
 # VERSION
 
+our %cli_attrs = do {
+    my $p = '/WWW/PAUSE/Simple/';
+
+    (
+        summary => 'A CLI for PAUSE',
+        url => '/WWW/PAUSE/Simple/',
+        subcommands => {
+            upload       => { url => "${p}upload_files" },
+            list         => { url => "${p}list_files" },
+            ls           => { url => "${p}list_files" },
+            "list-dists" => { url => "${p}list_dists" },
+            "list-mods"  => { url => "${p}list_modules" },
+            delete       => { url => "${p}delete_files" },
+            rm           => { url => "${p}delete_files" },
+            undelete     => { url => "${p}undelete_files" },
+            reindex      => { url => "${p}reindex_files" },
+            #password     => { url => "${p}set_password" },
+            #'account-info' => { url => "${p}set_account_info" },
+            cleanup      => { url => "${p}delete_old_releases" },
+        },
+        log => 1,
+        # since we are also run as pause-unpacked or pause-fatpacked, but want
+        # to share config, we hardcode the name here
+        config_filename => ['pause.conf'],
+    );
+};
+
 1;
 # ABSTRACT: A CLI for PAUSE
 
 =head1 DESCRIPTION
+
+See included script L<pause>.
 
 
 =head1 SEE ALSO
